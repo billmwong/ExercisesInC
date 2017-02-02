923579d6b96a175f551a3842e76e12e8f53afc02
billmwong <billmwong@gmail.com>
2017-02-02 13:37:44 -0500

    refactored cards

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int get_count_diff(int val) {
+  if ((val > 2) && (val < 7)) {
+    return 1;
+  } else if (val == 10) {
+    return -1;
+  }
+  return 0;
+}
+
+int get_card_val(char* card_name)
+{
+  switch(card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      return 10;
+    case 'A':
+      return 11;
+    default:
+      return atoi(card_name);
+  }
+}
+
 int main()
 {
   char card_name[3];
@@ -7,30 +31,19 @@ int main()
   while (card_name[0] != 'X') {
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
-    int val = 0;
-    switch(card_name[0]) {
-      case 'K':
-      case 'Q':
-      case 'J':
-        val = 10;
-        break;
-      case 'A':
-        val = 11;
-        break;
-      case 'X':
-        continue;
-      default:
-        val = atoi(card_name);
-        if ((val < 1) || (val > 10)) {
-          puts("I don't understand that value!");
-          continue;
-        }
+    if (card_name[0] == 'X') {
+      continue;
     }
-    if ((val > 2) && (val < 7)) {
-      count++;
-    } else if (val == 10) {
-      count--;
+
+    int val = get_card_val(card_name);
+
+    if ((val < 1) || ((val > 10) && (card_name[0] != 'A'))) {
+      puts("I don't understand that value!");
+      continue;
     }
+
+    count += get_count_diff(val);
+
     printf("Current count: %i\n", count);
   }
   return 0;
