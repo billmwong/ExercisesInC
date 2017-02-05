e604f96fa0ad4e7fd99c0c4f8999de68357f3b4b
billmwong <billmwong@gmail.com>
2017-02-05 16:33:26 -0500

    completed ex02

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -17,30 +17,31 @@ int *foo() {
     //printf("%p\n", array);
 
     for (i=0; i<SIZE; i++) {
-	array[i] = 42;
+	array[i] = 42; // Sets every value to 42
     }
-    return array;
+    return array; // Returns a pointer
 }
 
 void bar() {
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    printf("%p\n", array); // This points to the address of the previous array
 
     for (i=0; i<SIZE; i++) {
-	array[i] = i;
+    array[i] = i;
     }
 }
 
 int main()
 {
     int i;
-    int *array = foo();
+    int *array = foo(); // array is assigned to a pointer referencing the array
+    // inside the function, which is destroyed after foo() is finished.
     bar();
 
     for (i=0; i<SIZE; i++) {
-	printf("%d\n", array[i]);
+    printf("%d\n", array[i]);
     }
 
     return 0;
